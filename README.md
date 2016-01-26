258ab0c890c568ba21900fb29f7efbf095516f14
dnomak <me@dnomak.com>
2016-01-27 00:14:02 +0200

    güncelleme

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # [SVG Türkiye Haritası](http://dnomak.com/svg-turkiye-haritasi/)
 
-SVG formatında Türkiye Haritası
-
-Özellikler:
- - İstanbul Avrupa ve Anadolu yakası ayrı ayrı işaretli,
- - Kıbrıs da var,
- - Responsive :)
+Bu SVG Türkiye Haritası'nda İstanbul Asya ve Avrupa ayrı Kıbrıs'ta var hemde Responsive :)
 
 ## Önizleme
  - [Önizleme](http://dnomak.com/svg-turkiye-haritasi/)
- - [Gerçek Kullanım](http://seramiksan.com.tr/bayiler)
+ - [Harita Kaynağı](https://commons.wikimedia.org/wiki/File:Turkey_provinces_blank_gray.svg)
 
-## Lisans
+##Lisans
 - SVG Türkiye Haritası MIT Lisansı altında lisanslanmıştır.
   - [http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)
--- js/svg-turkiye-haritasi.js
+++ js/svg-turkiye-haritasi.js
@@ -1,22 +1,22 @@
 /*! SVG Türkiye Haritası | jQuery | MIT Lisans | dnomak.com */
 
-function turkiyeharitasi() {
-  $("#turkiye-haritasi path").hover(function() {
+function svgturkiyeharitasi() {
+  $("#svg-turkiye-haritasi path").hover(function() {
     if ($(this).parent().attr("id") == "guney-kibris") return false;
-    $(".il-tooltip").html("<div>" + $(this).parent().data("iladi") + "</div>");
+    $(".il-isimleri").html("<div>" + $(this).parent().data("iladi") + "</div>");
     $(this).on("mousemove", function(event) {
-      $(".il-tooltip").css("top", (event.pageY + 25));
-      $(".il-tooltip").css("left", event.pageX);
+      $(".il-isimleri").css("top", (event.pageY + 25));
+      $(".il-isimleri").css("left", event.pageX);
     });
   },function(){
-    $(".il-tooltip").html("");
+    $(".il-isimleri").html("");
   });
-  $("#turkiye-haritasi path").on("click", function(event) {
+  $("#svg-turkiye-haritasi path").on("click", function(event) {
     if ($(this).parent().attr("id") == "guney-kibris") return false;
     var ilid = $(this).parent().attr("id");
     var iladi = $(this).parent().data("iladi");
     var plakakodu = $(this).parent().data("plakakodu");
     var alankodu = $(this).parent().data("alankodu");
-    window.location = "http://dnomak.com/" + ilid + "-" + plakakodu + ".html";
+    window.location = "#" + ilid + "-" + plakakodu;
   });
 }
