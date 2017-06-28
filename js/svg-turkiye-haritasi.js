2c8d9b969906d94bab6036170e44c1c7d32668de
Fatih Erikli <fatiherikli@gmail.com>
2017-06-28 11:49:46 -0400

    Remove jQuery dependency

--- js/svg-turkiye-haritasi.js
+++ js/svg-turkiye-haritasi.js
@@ -1,22 +1,57 @@
 /*! SVG Türkiye Haritası | jQuery | MIT Lisans | dnomak.com */
 
 function svgturkiyeharitasi() {
-  $("#svg-turkiye-haritasi path").hover(function() {
-    if ($(this).parent().attr("id") == "guney-kibris") return false;
-    $(".il-isimleri").html("<div>" + $(this).parent().data("iladi") + "</div>");
-    $(this).on("mousemove", function(event) {
-      $(".il-isimleri").css("top", (event.pageY + 25));
-      $(".il-isimleri").css("left", event.pageX);
-    });
-  },function(){
-    $(".il-isimleri").html("");
-  });
-  $("#svg-turkiye-haritasi path").on("click", function(event) {
-    if ($(this).parent().attr("id") == "guney-kibris") return false;
-    var ilid = $(this).parent().attr("id");
-    var iladi = $(this).parent().data("iladi");
-    var plakakodu = $(this).parent().data("plakakodu");
-    var alankodu = $(this).parent().data("alankodu");
-    window.location = "#" + ilid + "-" + plakakodu;
-  });
+  const element = document.querySelector('#svg-turkiye-haritasi');
+  const info = document.querySelector('.il-isimleri')
+
+  element.addEventListener(
+    'mouseover',
+    function (event) {
+      if (event.target.tagName === 'path') {
+        info.innerHTML = [
+          '<div>',
+          event.target.parentNode.dataset['iladi'],
+          '</div>'
+        ].join('');
+      }
+    }
+  );
+
+  element.addEventListener(
+    'mousemove',
+    function (event) {
+      info.style.top = event.pageY + 25 + 'px';
+      info.style.left = event.pageX + 'px';
+    }
+  );
+
+  element.addEventListener(
+    'mouseout',
+    function (event) {
+      info.innerHTML = '';
+    }
+  );
+
+  element.addEventListener(
+    'click',
+    function (event) {
+      if (event.target.tagName === 'path') {
+        const parent = event.target.parentNode;
+        const id = parent.getAttribute('id');
+
+        if (
+          id === 'guney-kibris'
+        ) {
+          return;
+        }
+
+        window.location.href = (
+          '#'
+          + id
+          + '-'
+          + parent.dataset.plakakodu
+        );
+      }
+    }
+  );
 }
